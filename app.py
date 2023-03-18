287a221dc24b700368e7e6faad1b1bb93d28f75f
vennu-venu <vennuvenu2392@gmail.com>
2023-03-18 08:50:02 +0530

    getting-asana-response

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, Response, Request, stream_with_context
+from flask import Flask, jsonify, render_template, Response, Request, stream_with_context
 import cv2
 import numpy as np
 import mediapipe as mp
 import time
-
-import frame_generation
 import data_preprocessing
 import asana_classification
 from PIL import Image
@@ -12,25 +10,68 @@ from PIL import Image
 app = Flask(__name__)
 
 skeleton_extractor = data_preprocessing.SkeletonExtraction()
+
 camera = cv2.VideoCapture(0)
-pose = "Not yet detected"
+
+predicted_pose = "Not yet detected"
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def gen_processed_video_frames():
+  while True:
+      success, frame = camera.read()
+      if not success:
+          break
+      else:
+          frame = skeleton_extractor.getSkeleton(frame)
+          frame = data_preprocessing.resize_with_padding(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert("L"), (224, 224))
+          frame = np.array(frame)
+          global predicted_pose
+          predicted_pose = mapper(asana_classification.classify(frame))
+          time.sleep(0.5)
+          ret, buffer = cv2.imencode('.jpg', frame)
+          frame = buffer.tobytes()
+          yield (b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def mapper(pose):
+    if pose == 0:
+        return "Downdog"
+    if pose == 1:
+        return "Goddess"
+    if pose == 2:
+        return "Plank"
+    if pose == 3:
+        return "Tree"
+    if pose == 4:
+        return "Warrior"
+
     
 @app.route('/')
 def index():
-    return render_template('index.html', pose = pose)
+    return render_template('index.html')
 
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(frame_generation.gen_video_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/processed_video_feed')
 def processed_video_feed():
-    return Response(frame_generation.gen_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_processed_video_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/classify')
-def classify():
-    return Response(frame_generation.classify(camera), mimetype='text/plain')
+@app.route('/get-pose')
+def get_pose():
+    return jsonify(pose = predicted_pose)
 
 if __name__ == '__main__':
     app.run()
