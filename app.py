aba1c0bcefbeeaa1097d3723435c6bbe44b805d9
vennu-venu <vennuvenu2392@gmail.com>
2023-03-21 22:02:59 +0530

    basic-ui

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ camera = cv2.VideoCapture(0)
 
 predicted_pose = "Not yet detected"
 
+
 def gen_frames():
     while True:
         success, frame = camera.read()
@@ -26,22 +27,25 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 def gen_processed_video_frames():
-  while True:
-      success, frame = camera.read()
-      if not success:
-          break
-      else:
-          frame = skeleton_extractor.getSkeleton(frame)
-          frame = data_preprocessing.resize_with_padding(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert("L"), (224, 224))
-          frame = np.array(frame)
-          global predicted_pose
-          predicted_pose = mapper(asana_classification.classify(frame))
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            frame = skeleton_extractor.getSkeleton(frame)
+            frame = data_preprocessing.resize_with_padding(Image.fromarray(
+                cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert("L"), (224, 224))
+            frame = np.array(frame)
+            global predicted_pose
+            predicted_pose = mapper(asana_classification.classify(frame))
         #   time.sleep(0.5)
-          ret, buffer = cv2.imencode('.jpg', frame)
-          frame = buffer.tobytes()
-          yield (b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 def mapper(pose):
     if pose == 0:
@@ -55,23 +59,31 @@ def mapper(pose):
     if pose == 4:
         return "Warrior"
 
-    
-@app.route('/')
+
+@app.route('/home')
 def index():
     return render_template('index.html')
+
+
+@app.route('/asanas')
+def asanas():
+    return render_template('asanas.html')
 
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route('/processed_video_feed')
 def processed_video_feed():
     return Response(gen_processed_video_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route('/get-pose')
 def get_pose():
-    return jsonify(pose = predicted_pose)
+    return jsonify(pose=predicted_pose)
+
 
 if __name__ == '__main__':
     app.run()
